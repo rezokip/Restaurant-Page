556fdfbb64671aeeea217ab85a615c7952c7bcb3
ozerkip <ozerkip@hotmail.de>
2022-10-08 18:33:25 +0200

    JS Class for create, append and push rdy

--- kontakt.js
+++ kontakt.js
@@ -1,14 +1,17 @@
-let pageEl = document.querySelector('.page')
-
 
 class ElementClass {
+ 
   constructor(element, parent, className, text){
     this.element = element
     this.parent = parent
     this.className = className
     this.text = text      
-    this.newDomEl = this.domElement()  
-    parent.append(this.newDomEl)
+    this.domEl = this.domElement()   
+    this.childs = []
+    if(parent === content){ parent.append(this.domEl)}
+    else {parent.domEl.append(this.domEl)}  
+    this.pushToParent() 
+  
   }
   domElement(){
     let newDomElement = document.createElement(this.element)
@@ -16,17 +19,26 @@ class ElementClass {
     if(this.text){newDomElement.textContent=this.text}
     return newDomElement
   }  
+  pushToParent(){
+    if(this.parent.childs){
+      this.parent.childs.push(this)
+    }
+  }
 }
 
 
-  let openingHours = new ElementClass('div', pageEl, 'opening-hours', ) 
-  openingHours.childs = [
-  title = new ElementClass('h2', openingHours.newDomEl, 'opening-hours-title', 'Öffnungszeiten'),
-  monday = new ElementClass('p', openingHours.newDomEl, 'opening-hours-monday', 'Montag: 12 Uhr - 21 Uhr'),
-  tuesday = new ElementClass('p', openingHours.newDomEl, 'opening-hours-tuesday', 'Dienstag: 12 Uhr - 21 Uhr'),
-  wednesday = new ElementClass('p', openingHours.newDomEl, 'opening-hours-monday', 'Mittwoch: 12 Uhr - 21 Uhr'),
-  thursday = new ElementClass('p', openingHours.newDomEl, 'opening-hours-monday', 'Donnerstag: 12 Uhr - 21 Uhr'),
-  friday = new ElementClass('p', openingHours.newDomEl, 'opening-hours-monday', 'Freitag: 12 Uhr - 22 Uhr'),
-  saturday = new ElementClass('p', openingHours.newDomEl, 'opening-hours-monday', 'Samstag: 12 Uhr - 22 Uhr'),
-  sunday = new ElementClass('p', openingHours.newDomEl, 'opening-hours-monday', 'Sonntag: 12 Uhr - 21 Uhr'),
-  ]
+let content = document.querySelector('#content')
+let page = new ElementClass('div', content, 'page', 'test')
+
+
+
+let openingHours = new ElementClass('div', page, 'opening-hours', ) 
+let openingTitle = new ElementClass('h2', openingHours, 'opening-hours-title', 'Öffnungszeiten')
+let monday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Montag: 12 Uhr - 21 Uhr')
+let tuesday = new ElementClass('p', openingHours, 'opening-hours-tuesday', 'Dienstag: 12 Uhr - 21 Uhr')
+let wednesday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Mittwoch: 12 Uhr - 21 Uhr')
+let thursday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Donnerstag: 12 Uhr - 21 Uhr')
+let friday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Freitag: 12 Uhr - 22 Uhr')
+let saturday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Samstag: 12 Uhr - 22 Uhr')
+let sunday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Sonntag: 12 Uhr - 21 Uhr')
+
