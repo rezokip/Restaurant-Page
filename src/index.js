3308afe369badf9437f38e6c36e608e70f9c234a
ozerkip <ozerkip@hotmail.de>
2022-10-08 20:57:41 +0200

    basic page without img

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-class ElementClass {
+
+import { kontaktPageContent } from "./kontakt.js"
+export {ElementClass, page}
+
+class ElementClass { 
   constructor(element, parent, className, text){
     this.element = element
     this.parent = parent
     this.className = className
     this.text = text      
-    this.domEl = this.domElement()  
-    parent.append(this.domEl)
+    this.domEl = this.domElement()   
+    this.childs = []
+    if(parent === content){ parent.append(this.domEl)}
+    else {parent.domEl.append(this.domEl)}  
+    this.pushToParent()   
   }
   domElement(){
     let newDomElement = document.createElement(this.element)
@@ -13,39 +20,62 @@ class ElementClass {
     if(this.text){newDomElement.textContent=this.text}
     return newDomElement
   }  
+  pushToParent(){
+    if(this.parent.childs){
+      this.parent.childs.push(this)
+    }
+  }
 }
 
 
+
 const content = document.querySelector('#content')
-
 let header = new ElementClass('div', content, 'header')
-header.childs = [
-  home = new ElementClass('div', header.domEl, 'home', 'HOME'),
-  menu = new ElementClass('div', header.domEl, 'menu', 'MENU'),
-  kontakt = new ElementClass('div', header.domEl, 'kontakt', 'KONTAKT'),
-]
+let home = new ElementClass('div', header, 'home', 'HOME')
+let menu = new ElementClass('div', header, 'menu', 'MENU')
+let kontakt = new ElementClass('div', header, 'kontakt', 'KONTAKT')
+let page =  new ElementClass('div', content, 'page', )  
 
-let page = new ElementClass('div', content, 'page')
-let descriptionFirst = new ElementClass('p', page.domEl, 'description-first', 'Wir in Nürnberg, im herzen der Südstadt können auf eine über 10 Jährige Erfahrung zurückblicken. Unser Ziel ist es mit unserer langjährigen Erfahrung euch das beste und optimale Geschmackserlebnis zu bieten. Cigköfte reicht auf eine traditionelle Herkunft von mehreren hundert Jahren zurück. Es ist eine herzhafte Paste, die aus Bulgur (Weizen), Tomatenmark, Paprikamark, zahlreiche Gewürze, gemahlene Nüsse und weitere Bestandteile besteht. Diese natürlichen Zutaten bieten nicht zuletzt deshalb einen exzellenten Geschmack' )
-let descriptionSecond = new ElementClass('p', page.domEl, 'description-second','Bei uns in Nürnberg kannst du dir die türkische Spezialität im Dürüm oder Portionsweise mit frischem Salat, Lavashbrot und Soße schmecken lassen, wahlweise auch zum mitnehmen. Neben Cigköfte bieten wir selbstverständlich viele verschiedene Vegane Spezialitäten an. Unser Hausgemachtes, Veganes Käsekuchen ist das Highlight für Naschkatzen. Ob Veganer oder nicht Veganer lass dich von unseren köstlichen Speisen überzeugen.')
-let openingHours = new ElementClass('div', page.domEl, 'opening-hours', ) 
-openingHours.childs = [
-  title = new ElementClass('h2', openingHours.domEl, 'opening-hours-title', 'Öffnungszeiten'),
-  monday = new ElementClass('p', openingHours.domEl, 'opening-hours-monday', 'Montag: 12 Uhr - 21 Uhr'),
-  tuesday = new ElementClass('p', openingHours.domEl, 'opening-hours-tuesday', 'Dienstag: 12 Uhr - 21 Uhr'),
-  wednesday = new ElementClass('p', openingHours.domEl, 'opening-hours-monday', 'Mittwoch: 12 Uhr - 21 Uhr'),
-  thursday = new ElementClass('p', openingHours.domEl, 'opening-hours-monday', 'Donnerstag: 12 Uhr - 21 Uhr'),
-  friday = new ElementClass('p', openingHours.domEl, 'opening-hours-monday', 'Freitag: 12 Uhr - 22 Uhr'),
-  saturday = new ElementClass('p', openingHours.domEl, 'opening-hours-monday', 'Samstag: 12 Uhr - 22 Uhr'),
-  sunday = new ElementClass('p', openingHours.domEl, 'opening-hours-monday', 'Sonntag: 12 Uhr - 21 Uhr'),
-  ]
+let homePageContent=function(){
+  let descriptionFirst = new ElementClass('p', page, 'description-first', 'Wir in Nürnberg, im herzen der Südstadt können auf eine über 10 Jährige Erfahrung zurückblicken. Unser Ziel ist es mit unserer langjährigen Erfahrung euch das beste und optimale Geschmackserlebnis zu bieten. Cigköfte reicht auf eine traditionelle Herkunft von mehreren hundert Jahren zurück. Es ist eine herzhafte Paste, die aus Bulgur (Weizen), Tomatenmark, Paprikamark, zahlreiche Gewürze, gemahlene Nüsse und weitere Bestandteile besteht. Diese natürlichen Zutaten bieten nicht zuletzt deshalb einen exzellenten Geschmack' )
+  let descriptionSecond = new ElementClass('p', page, 'description-second','Bei uns kannst du dir die türkische Spezialität im Dürüm oder Portionsweise mit frischem Salat, Lavashbrot und Soße schmecken lassen, wahlweise auch zum mitnehmen. Neben Cigköfte bieten wir selbstverständlich viele verschiedene Vegane Spezialitäten an. Unser Hausgemachtes, Veganes Käsekuchen ist das Highlight für Naschkatzen. Ob Veganer oder nicht Veganer lass dich von unseren köstlichen Speisen überzeugen.') 
+  
+  let openingHours = new ElementClass('div', page, 'opening-hours', ) 
+  let openingTitle = new ElementClass('h2', openingHours, 'opening-hours-title', 'Öffnungszeiten')
+  let monday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Montag: 12 Uhr - 21 Uhr')
+  let tuesday = new ElementClass('p', openingHours, 'opening-hours-tuesday', 'Dienstag: 12 Uhr - 21 Uhr')
+  let wednesday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Mittwoch: 12 Uhr - 21 Uhr')
+  let thursday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Donnerstag: 12 Uhr - 21 Uhr')
+  let friday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Freitag: 12 Uhr - 22 Uhr')
+  let saturday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Samstag: 12 Uhr - 22 Uhr')
+  let sunday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Sonntag: 12 Uhr - 21 Uhr')
 
-page.childs = [
- 
+  let location = new ElementClass('div', page, 'location')
+  let locationTitle = new ElementClass('h2', location, 'location-title', 'SCHAUT VORBEI')
+  let locationName = new ElementClass('p', location, 'location-name', 'Cigköftem Nürnberg')
+  let locationAdress = new ElementClass('p', location, 'locatin-adress', 'Schweiggerstraße 3')
+  let locationPostCode = new ElementClass('p', location, 'locatin-postcode', '90478 Nürnberg')
+  let locationTel = new ElementClass('p', location, 'locatin-tel', 'Tel: 0911 99 44 91 55')
+
+}  
+
+homePageContent()
+home.domEl.addEventListener('click', function(){
+  page.domEl.textContent= ''
+  kontaktPageContent()
+})
+
+menu.domEl.addEventListener('click', function(){
+  page.domEl.textContent=''
+  homePageContent()
+})
+
+
+
 
   
   
 
-]
+
 
 
--- src/kontakt.js
+++ src/kontakt.js
@@ -1,37 +1,8 @@
+import {ElementClass, page} from './index.js'
 
-class ElementClass {
+
+let kontaktPageContent= function(){
  
-  constructor(element, parent, className, text){
-    this.element = element
-    this.parent = parent
-    this.className = className
-    this.text = text      
-    this.domEl = this.domElement()   
-    this.childs = []
-    if(parent === content){ parent.append(this.domEl)}
-    else {parent.domEl.append(this.domEl)}  
-    this.pushToParent() 
-  
-  }
-  domElement(){
-    let newDomElement = document.createElement(this.element)
-    if(this.className){newDomElement.classList.add(this.className)}
-    if(this.text){newDomElement.textContent=this.text}
-    return newDomElement
-  }  
-  pushToParent(){
-    if(this.parent.childs){
-      this.parent.childs.push(this)
-    }
-  }
-}
-
-
-let content = document.querySelector('#content')
-let page = new ElementClass('div', content, 'page', 'test')
-
-
-
 let openingHours = new ElementClass('div', page, 'opening-hours', ) 
 let openingTitle = new ElementClass('h2', openingHours, 'opening-hours-title', 'Öffnungszeiten')
 let monday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Montag: 12 Uhr - 21 Uhr')
@@ -41,4 +12,6 @@ let thursday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Donn
 let friday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Freitag: 12 Uhr - 22 Uhr')
 let saturday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Samstag: 12 Uhr - 22 Uhr')
 let sunday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Sonntag: 12 Uhr - 21 Uhr')
+}
 
+export {kontaktPageContent}
