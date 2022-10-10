e8bfd8517bdaaa4b58a8cd595e75b1e9d79c7002
ozerkip <ozerkip@hotmail.de>
2022-10-10 19:14:29 +0200

    added my contact information

--- src/kontakt.js
+++ src/kontakt.js
@@ -1,18 +1,21 @@
 import {ElementClass, page} from './index.js'
+import githublogo from './logos/githublogo.png'
 
 // first reset the page content with empty textconent and then
 // use the imported element class to create the elements for the kontakt page content
 let kontaktPageContent= function(){
 page.domEl.textContent='' 
-const openingHours = new ElementClass('div', page, 'opening-hours', ) 
-const openingTitle = new ElementClass('h2', openingHours, 'opening-hours-title', 'Öffnungszeiten')
-const monday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Montag: 12 Uhr - 21 Uhr')
-const tuesday = new ElementClass('p', openingHours, 'opening-hours-tuesday', 'Dienstag: 12 Uhr - 21 Uhr')
-const wednesday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Mittwoch: 12 Uhr - 21 Uhr')
-const thursday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Donnerstag: 12 Uhr - 21 Uhr')
-const friday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Freitag: 12 Uhr - 22 Uhr')
-const saturday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Samstag: 12 Uhr - 22 Uhr')
-const sunday = new ElementClass('p', openingHours, 'opening-hours-monday', 'Sonntag: 12 Uhr - 21 Uhr')
+const contactContainer = new ElementClass('div', page, 'contact-container')
+const contactTitle = new ElementClass('h2', contactContainer, 'contact-title', 'Contact me')
+const contactPerson = new ElementClass('div', contactContainer, 'contact-person', 'Özer Kip')
+const contactMailCont = new ElementClass('div', contactContainer, 'contact-mail-cont')
+const contactMail = new ElementClass('a', contactMailCont, 'contact-mail', 'ozerkip@hotmail.de')
+contactMail.domEl.href ='mailto:ozerkip@hotmail.de'
+const contactGitHubCont = new ElementClass('div', contactContainer, 'contact-github-cont')
+const contactGitHubLink = new ElementClass('a', contactGitHubCont, 'contact-github-link')
+contactGitHubLink.domEl.href = 'https://github.com/rezokip'
+const contactGitHubLogo = new ElementClass('img', contactGitHubLink, 'contact-github-log')
+contactGitHubLogo.domEl.src=githublogo
 }
 
 // export the kontakt page content
